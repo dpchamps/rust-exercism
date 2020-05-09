ef3f03936571d795b93a2b916594e8993220af81
David Campion <David.Campion@walmartlabs.com>
2020-05-08 17:37:44 -0700

    Roman Numeral Fun

---
+++ roman-numerals/src/lib.rs
@@ -0,0 +1,85 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::fmt::{Display, Formatter};
+use std::iter::Iterator;
+
+lazy_static! {
+    static ref base_divisors: Vec<(u32, &'static str)> = vec![
+        (1, "I"),
+        (4, "IV"),
+        (5, "V"),
+        (9, "IX"),
+        (10, "X"),
+        (40, "XL"),
+        (49, "IL"),
+        (50, "L"),
+        (90, "XC"),
+        (99, "IC"),
+        (100, "C"),
+        (400, "CD"),
+        (499, "ID"),
+        (500, "D"),
+        (900, "CM"),
+        (999, "IM"),
+        (1000, "M"),
+    ];
+}
+
+pub struct Roman {
+    numeral: String,
+}
+
+impl Roman {
+    fn calculate_numeral(number: u32, divisors: &[(u32, &'static str)]) -> Result<String, ()> {
+        if divisors.len() == 0 || number == 0 {
+            return Ok(String::new());
+        }
+
+        if let Some((divisor, numeral)) = divisors.last() {
+            if *divisor > number {
+                return Roman::calculate_numeral(number, &divisors[0..divisors.len() - 1]);
+            }
+
+            return Ok(format!(
+                "{}{}",
+                numeral,
+                Roman::calculate_numeral(number - *divisor, divisors)?
+            ));
+        }
+
+        Err(())
+    }
+}
+
+impl Display for Roman {
+    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(_f, "{}", self.numeral)
+    }
+}
+
+impl From<u32> for Roman {
+    fn from(num: u32) -> Self {
+        Roman {
+            numeral: Roman::calculate_numeral(num, &base_divisors[..]).unwrap(),
+        }
+    }
+}
+
+// Iterative Version:
+
+// let mut remainder = num;
+//
+// let numeral = base_divisors.iter().rev().fold(String::from(""), |numeral, &(divisor, base_numeral)|{
+//     let mut sub_result = String::from("");
+//     loop {
+//         if divisor > remainder{
+//             break;
+//         }
+//
+//         remainder -= divisor;
+//         sub_result = format!("{}{}", sub_result, base_numeral);
+//     }
+//
+//     format!("{}{}", numeral, sub_result)
+// });
